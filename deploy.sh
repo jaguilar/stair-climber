422433628bb85cb7576c5ba5905120fa2ac0d280
jaguilar <799564+jaguilar@users.noreply.github.com>
2024-03-10 16:26:11 -0400

    Add some calbratoin code and tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
+if uname -a | grep "microsoft.*WSL2" >/dev/null; then
+    echo aliasing ssh
+    export SSH="ssh.exe" 
+    export SCP="scp.exe"
+else
+    export SSH="ssh" 
+    export SCP="scp"
+fi
+
 SSH_TARGET="robot@ev3dev.local"
 PROJECT_DIR=/home/robot/stair_climber
-ssh "$SSH_TARGET" "mkdir -p $PROJECT_DIR"
-scp *.py "$SSH_TARGET:$PROJECT_DIR"
-ssh robot@ev3dev.local
+$SSH "$SSH_TARGET" "mkdir -p $PROJECT_DIR"
+$SCP *.py "$SSH_TARGET:$PROJECT_DIR"
+$SSH -t "$SSH_TARGET" "cd $PROJECT_DIR && brickrun -r -- pybricks-micropython main.py"
--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 #! /usr/bin/env pybricks-micropython
 
 from pybricks.ev3devices import Motor, TouchSensor, GyroSensor
-from pybricks.parameters import Port
+from pybricks.parameters import Port,Direction, Stop
 from pybricks.tools import wait
 
 motor_front = Motor(Port.B)
 motor_back = Motor(Port.A)
-motor_lift = Motor(Port.D)
+motor_lift = Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 
 sensor_gryo = GyroSensor(Port.S2)
 sensor_lift = TouchSensor(Port.S3)
 
 
 def calibrate():
+    # TODO: jaguilar - Run to the sensor, then calculuate the position of 0 height instead of
+    # using RunUntilStalled.
+    motor_lift.run_until_stalled(-180, duty_limit=40, then=Stop.HOLD)
+    motor_lift.reset_angle(0)
+    motor_lift.run(180)
+    while not sensor_lift.pressed():
+        wait(5)
+    motor_lift.stop()
+    print("sensor_pressed")
+    throw = motor_lift.angle()
+    print(throw)
+    motor_lift.run_angle(-720, 0, then=Stop.HOLD)
+    wait(50)
 
-    
-    pass
+def main():
+    calibrate()
+
+if __name__ == '__main__':
+    main()
